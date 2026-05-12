968c0f9249394294405c836f16062fed721fcf0a
Nilton Revolledo <niltonrevolledo@gmail.com>
2026-05-11 22:21:09 -0500

    agregadaz nuevaz funcionez backend, dazhboard, dirección de facturación y errorez de entrega web5

--- frontend/app/(auth)/dashboard/hosting/page.tsx
+++ frontend/app/(auth)/dashboard/hosting/page.tsx
@@ -32,7 +32,7 @@ import {
 
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import {
   Dialog,
   DialogContent,
